2443cac895817c264dfb9b313b1014eb008c77f1
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2022-10-21 16:28:03 -0600

    Dispatch the data fetch from api and create a reducer

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -7,3 +7,20 @@ const fetchGreetings = async () => {
   const data = await response.json();
   return data.message;
 };
+
+export const getGreetings = () => async (dispatch) => {
+  const data = await fetchGreetings();
+  dispatch({
+    type: FETCH_GREETINGS,
+    payload: data,
+  });
+};
+
+export default function reducerGreetings(state = [], action) {
+  switch (action.type) {
+    case FETCH_GREETINGS:
+      return { ...state, greetings: action.payload };
+    default:
+      return state;
+  }
+}
